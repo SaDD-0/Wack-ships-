3107ea18475cfbd22b945d3c983c9adc4488e7de
Wrmorales <wrmorale@ucsc.edu>
2022-04-07 17:43:17 -0700

    added assets, and some code for rockets and scenes

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,38 @@
+//Rocket prefab 
+class Rocket extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture, frame){
+        super(scene, x, y, texture, frame);
+
+        // add object to existiting scene
+        scene.add.existing(this);
+
+        this.isFiring = false;
+        this.moveSpeed = 2;
+    }
+
+    update(){
+        // left/right movement
+        if(!this.isFiring){
+            if(keyLEFT.isDown && this.x >= borderUIsize + this.width){
+                this.x -= this.moveSpeed;
+            }else if(keyRIGHT.isDown && this.x <= game.config.width - borderUIsize - this.width){
+                this.x -= this.moveSpeed
+            }
+
+        }
+        //fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
+            this.isFiring = true;
+        }
+        //if fired, move up
+        if(this.isFiring && this.y >= borderUIsize * 3 + borderPadding){
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if(this.y <= borderUIsize * 3 + borderPadding){
+            this.isFiring = false;
+            this.y = game.config.height - borderUIsize - borderPadding;
+        }
+    }
+}
+
